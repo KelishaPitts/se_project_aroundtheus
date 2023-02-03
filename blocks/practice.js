38898ed6b3b8074a805a89a52e06032bc43b6386
Kelisha Pitts <kelishapitts@protonmail.com>
2023-02-02 22:10:52 -0500

    update

---
+++ blocks/practice.js
@@ -0,0 +1,10 @@
+for (let i = 0; i <= 100; i++){
+    if (i % 2 == 0) {
+      console.log(`${i} is even`);
+      i = i+1;
+    } 
+    else{
+      console.log(`${i} is odd`);
+      i = i+1;
+    }
+  }
---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+const initialCards = [
+    Yosmite = {
+        name: "Yosmite Valley",
+        "link": url("https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg")
+    },
+    Louise = {
+        name: "Lake Louise",
+        "link": url("https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg")
+    },
+    Bald = {
+        name: "Bald Mountains",
+        "link": url("https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg")
+    },
+    Latemar = {
+        name: "Latemar",
+        "link": url("https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg")
+    },
+    Vanoise = {
+        name: "Vanoise National Park",
+        "link": url("https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg")
+    },
+    Lago = {
+        name: "Lago di Braies",
+        "link": url("https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg")
+    }
+]

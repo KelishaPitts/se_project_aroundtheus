3667e237485887b9209bf54e1f7107b2313c3049
Kelisha Pitts <kelishapitts@protonmail.com>
2023-02-05 23:29:52 -0500

    Update

--- scripts/index.js
+++ scripts/index.js
@@ -24,3 +24,17 @@ const initialCards = [
         link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg"
     }
 ];
+
+let addButton = document.querySelector(".profile__button-edit");
+let closeButton = document.querySelector(".modal__button-close");
+let popUp = document.querySelector(".modal");
+
+function openModal(){
+popUp.classList.add("modal-modal_opened");
+}
+function closeModal(){
+popUp.classList.remove("modal-modal_opened");
+}
+
+addButton.addEventListener("click", openModal);
+closeButton.addEventListener("click", closeModal);

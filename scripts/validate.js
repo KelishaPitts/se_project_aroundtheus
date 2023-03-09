babdd07e4255f7e7f108e849c9e7735877684bcb
Kelisha Pitts <kelishapitts@protonmail.com>
2023-03-08 22:32:13 -0500

    Corrections for Project 6

--- scripts/validate.js
+++ scripts/validate.js
@@ -90,6 +90,6 @@ enableValidation({
   inputSelector: ".form__input",
   submitButtonSelector: ".modal__button-submit",
   inactiveButtonClass: "modal__button-submit_inactive",
-  inputErrorClass: "form__input_error",
-  errorClass: "form__input_error_active",
+  inputErrorClass: "form__input_type_error",
+  errorClass: "form__error_active",
 });

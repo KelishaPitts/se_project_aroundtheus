e907259bb5b49bd649d154d15381bc355ef580a4
Kelisha Pitts <kelishapitts@protonmail.com>
2023-03-25 23:41:11 -0400

    Refactoring code

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -11,13 +11,13 @@ export default class FormValidator {
 
 
   _showInputError(inputElement){
-    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    const errorElement = this._formElement.querySelector(`.${inputElement.id}-error`);
     inputElement.classList.add(this._inputErrorClass);
     errorElement.textContent = inputElement.validity.valid;
     errorElement.classList.add(this._errorClass);
   }
   _hideInputError(inputElement){
-    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    const errorElement = this._formElement.querySelector(`.${inputElement.id}-error`);
     inputElement.classList.remove(this._inputErrorClass);
     errorElement.textContent = inputElement.validity.valid;
     errorElement.classList.remove(this._errorClass);
